d5b2b925a27aedacff1f3ebbdc3246c2d320e786
cool-mol <51240659+cool-mol@users.noreply.github.com>
2020-07-22 23:58:14 +0800

    refresh

--- src/config.ts
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-export default {
-    baseURL: 'http://localhost:8080', //baseURL
-}
-=======
-export default {
-    baseURL: 'http://localhost:8080', // The base url of the backend project
-}
->>>>>>> dev
