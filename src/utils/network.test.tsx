f16bb13da033ab84403de1728ae386905dfee7bc
Olivia Wu <fangnuowu@gmail.com>
2020-07-22 09:21:56 +0800

    test: modify localStorage part with the new module

--- src/utils/network.test.tsx
+++ src/utils/network.test.tsx
@@ -2,53 +2,40 @@ import {client} from './network'
 
 /*
 * 测试 interceptor 设置正确
-* 检查 client 在 localStorage 值不为 null时成功设置
+* 检查 client 在 localStorage 值为空和不为空时成功设置
 * @author wfn
 */
-describe('interceptor adds token to config if localStorage exists', () => {
-    beforeAll(()=>{
-        localStorage.setItem('csrf_token','token');
-        localStorage.setItem('access_token','token');
-    })
+describe('Client set correctly test', () => {
+    /*
+    * 检查 baseURL 和 withCredentials 设置成功
+    * @author wfn
+    */
+    it('should set baseURL and withCredentials correctly', () => {
+        expect(client.defaults.baseURL).toBe('http://localhost:8080');
+        expect(client.defaults.withCredentials).toBe(true);
+    });
 
     /*
     * 检查 client 在 localStorage 值不为 null时成功设置
     * @author wfn
     */
-    it('interceptor adds token to config',async () =>{
-        // const data = await client.get('/test');
+    it('should add token to headers if localStorage exist',async () =>{
+        localStorage.setItem('csrf_token','token');
+        localStorage.setItem('access_token','token');
+        // @ts-ignore
         const result = client.interceptors.request.handlers[0].fulfilled({headers: {}});
         expect(result.headers['X-CSRFToken']).toBe('token');
         expect(result.headers['AccessToken']).toBe('token');
     });
 
     /*
-    * 检查 baseURL 和 withCredentials 设置成功
-    * @author wfn
-    */
-    it('create successfully',()=>{
-        expect(client.defaults.baseURL).toBe('http://localhost:8080');
-        expect(client.defaults.withCredentials).toBe(true);
-    });
-})
-
-/*
-* 测试 interceptor 设置正确
-* 检查 client 在 localStorage 值为 null 时不设置 headers
-* @author wfn
-*/
-describe('interceptor adds token to config if localStorage not exists', () => {
-    beforeAll(()=>{
-        localStorage.removeItem('csrf_token');
-        localStorage.removeItem('access_token');
-    })
-
-    /*
     * 检查 client 在 localStorage 值为 null时不设置
     * @author wfn
     */
-    it('interceptor adds token to config',async () =>{
-        // const data = await client.get('/test');
+    it('should not add token to headers if localStorage is null',async () =>{
+        localStorage.removeItem('csrf_token');
+        localStorage.removeItem('access_token');
+        // @ts-ignore
         const result = client.interceptors.request.handlers[0].fulfilled({headers: {}});
         expect(result.headers['X-CSRFToken']).toBeUndefined();
         expect(result.headers['AccessToken']).toBeUndefined();
