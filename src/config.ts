d830d23856168f9af649a08e787c52005bf820df
fourstring <i@zby.io>
2020-07-30 23:05:54 +0800

    fix: jest dependency and config by environment

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,23 @@
 import {globalE2EMockClient} from './mocks/e2eClient'
 
-export default {
-  baseURL: 'https://api.fourstring.dev', // The base url of the backend project
-  globalE2EMock: false,
-  globalE2EMockClient: globalE2EMockClient,
-  jwtMonitorRate: 60000, //ms
-  jwtRefreshThreshold: 120, //s
-  shortenLinkBaseURL: 'https://api.fourstring.dev/s'
+let config: Record<string, any> = {};
+if (process.env["CI"]) {
+  config = {
+    baseURL: 'https://api.fourstring.dev', // The base url of the backend project
+    globalE2EMock: true,
+    globalE2EMockClient: globalE2EMockClient,
+    jwtMonitorRate: 60000, //ms
+    jwtRefreshThreshold: 120, //s
+    shortenLinkBaseURL: 'https://api.fourstring.dev/s'
+  }
+} else {
+  config = {
+    baseURL: 'https://api.fourstring.dev', // The base url of the backend project
+    globalE2EMock: false,
+    globalE2EMockClient: globalE2EMockClient,
+    jwtMonitorRate: 60000, //ms
+    jwtRefreshThreshold: 120, //s
+    shortenLinkBaseURL: 'https://api.fourstring.dev/s'
+  }
 }
+export default config;
