97b377ac2b2932d9ca5f886d24618a1e2f14e336
fourstring <i@zby.io>
2020-07-30 17:14:06 +0800

    tests: modify tests to merge into dev branch

--- src/mocks/e2eClient.ts
+++ src/mocks/e2eClient.ts
@@ -1,8 +1,17 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import {testUser} from "./testData";
+import {ILink} from "../types/ILink";
 
-const baseURL:string = 'http://localhost:8080';
+const baseURL: string = 'http://localhost:8080';
+export const testLink: ILink = {
+  id: 1,
+  user: testUser,
+  linkKey: "abcdefg.test.com",
+  href: "test.com",
+  createAt: "string",
+  updateAt: "string"
+};
 
 let globalE2EMockClient = axios.create({
   baseURL: baseURL,
@@ -15,12 +24,16 @@ Adapter.onGet(baseURL+'/auth/ping').reply(config => {
   return [200, testUser]
 });
 
-Adapter.onGet(baseURL+'/auth/logout').reply(config => {
+Adapter.onGet(baseURL + '/auth/logout').reply(config => {
   return [200]
 });
 
-Adapter.onPost(baseURL+'/auth/login').reply(config => {
+Adapter.onPost(baseURL + '/auth/login').reply(config => {
   return [200, {user: testUser, accessToken: "justTestToken", csrfToken: "justTestToken"}]
+});
+
+Adapter.onPost('/links', {user: 1, href: "test.com"}).reply(() => {
+  return [200, testLink];
 });
 
 export {globalE2EMockClient}
