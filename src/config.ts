2b43082d3c4814e6ed7f64a81602ed19c61acc0a
fourstring <i@zby.io>
2020-07-30 20:36:55 +0800

    fix: add register jwtMonitor after success login

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 import {globalE2EMockClient} from './mocks/e2eClient'
 
 export default {
-  baseURL: 'http://localhost:8080', // The base url of the backend project
-  globalE2EMock: true,
+  baseURL: 'https://api.fourstring.dev', // The base url of the backend project
+  globalE2EMock: false,
   globalE2EMockClient: globalE2EMockClient,
   jwtMonitorRate: 60000, //ms
   jwtRefreshThreshold: 120 //s
