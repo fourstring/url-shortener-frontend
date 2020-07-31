54928910189b5e1e6853fb95307486ac740e0200
fourstring <i@zby.io>
2020-07-31 15:26:11 +0800

    tests: fix some incorrect tests recipes

--- src/config.ts
+++ src/config.ts
@@ -24,7 +24,7 @@ if (process.env["CI"]) {
     baseURL: 'https://api.fourstring.dev', // The base url of the backend project
     globalE2EMock: true,
     globalE2EMockClient: globalE2EMockClient,
-    jwtMonitorRate: 60000, //ms
+    jwtMonitorRate: 10000, //ms
     jwtRefreshThreshold: 120, //s
     shortenLinkBaseURL: 'https://api.fourstring.dev/s'
   }
