36b98e281c75f6f27f0046a30edf9e51acc8158c
fourstring <i@zby.io>
2020-07-31 10:26:47 +0800

    feat: add production config

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,15 @@ if (process.env["CI"]) {
     jwtRefreshThreshold: 120, //s
     shortenLinkBaseURL: 'https://api.fourstring.dev/s'
   }
+} else if (process.env.NODE_ENV === 'production') {
+  config = {
+    baseURL: 'https://api.fourstring.dev', // The base url of the backend project
+    globalE2EMock: false,
+    globalE2EMockClient: globalE2EMockClient,
+    jwtMonitorRate: 60000, //ms
+    jwtRefreshThreshold: 120, //s
+    shortenLinkBaseURL: 'https://api.fourstring.dev/s'
+  }
 } else {
   config = {
     baseURL: 'https://api.fourstring.dev', // The base url of the backend project
