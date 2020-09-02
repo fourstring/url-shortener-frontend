cf08f5ec8d7e5fd364e2f6b38ea87b1e41266b6b
Olivia Wu <fangnuowu@gmail.com>
2020-09-02 21:13:06 +0800

    fix: add disable and linkKey feature

--- src/types/ILink.ts
+++ src/types/ILink.ts
@@ -4,10 +4,12 @@ import {IUser} from "./IUser";
 export interface ILinkInput {
   user: number;
   href: string;
+  linkKey?: string;
 }
 
 export interface ILink extends IEntity {
   user: IUser;
   linkKey: string;
   href: string;
+  disabled: boolean;
 }
